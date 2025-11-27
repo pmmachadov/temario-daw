8420f1f061ebfa0e05f7978dcd2ae006652533d2
pmmachadov <pmmachadov@gmail.com>
2025-11-27 15:24:34 +0100

    update

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -4,9 +4,9 @@ public class Calculadora {
 
     public static void main(String[] args) {
 
-        try (Scanner reciveDatos = new Scanner(System.in)){
+        try (Scanner reciveDatos = new Scanner(System.in)) {
             int primerEntero;
-            while(true) {
+            while (true) {
                 // placeholder - incomplete in original source
                 break;
             }
